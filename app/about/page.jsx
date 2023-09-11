c092a9add5c2dbaf23b1018ca9775c134af12002
SMTahaWaqar <syedtaha.hsn@gmail.com>
2023-09-11 11:33:35 +0500

    routing, nested routes

---
+++ app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+        <h1>About Me</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste officiis exercitationem assumenda illum obcaecati consequatur cupiditate voluptatum explicabo, magnam quia?</p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ app/about/team/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TeamPage = () => {
+  return (
+    <div>
+        <h1>Team</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero dolorum animi, similique commodi repellat, et laboriosam delectus quisquam quis deleniti, voluptatibus debitis. Quibusdam error consequuntur, tempore a vel distinctio eos dolorem veniam fuga, numquam, ipsam maxime suscipit. Accusantium explicabo recusandae mollitia quis. Dicta porro, aliquam expedita error eveniet cupiditate at!</p>
+    </div>
+  )
+}
+
+export default TeamPage
